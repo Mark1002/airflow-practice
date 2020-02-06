090415fd4a5137c0d55f8b9c409cb39bd43e3dc3
Mark Chang <ak0922747401@gmail.com>
2020-02-06 18:13:13 +0800

    update to set_up_bigscrapy_project_task and tear_down_bigscrapy_project_task

--- dags/crawler_report.py
+++ dags/crawler_report.py
@@ -1,36 +1,35 @@
 """Crawler report dags."""
 import pendulum
-import logging
+
 from datetime import datetime
 from airflow import DAG
 from airflow.contrib.operators.ssh_operator import SSHOperator
-from airflow.operators.python_operator import PythonOperator
 
 local_tz = pendulum.timezone('Asia/Taipei')
 
 args = {
     'owner': 'bignet',
-    'start_date': datetime(2020, 2, 4, tzinfo=local_tz)
+    'start_date': datetime(2020, 2, 5, tzinfo=local_tz)
 }
-
-
-def show_message(**kwargs):
-    """Show message."""
-    logging.debug('hello world!')
-
 
 with DAG(
     dag_id='crawler_report', default_args=args, schedule_interval='@daily'
 ) as dag:
-    start_message_task = PythonOperator(
-        task_id='start_message',
-        python_callable=show_message,
-        provide_context=True,
-        op_kwargs={'message': 'start crawler report'}
+    set_up_bigscrapy_project_task = SSHOperator(
+        ssh_conn_id='ssh_big_airflow',
+        task_id='set_up_bigscrapy_project',
+        command="""
+        docker run -d --name bigscrapy_projects_airflow \
+        bigregistry.buygta.today/bigscrapy_projects
+        """
     )
 
-    set_up_bigscrapy_project_task = SSHOperator(
-        ssh_conn_id='ssh_big_ariflow',
-        task_id='set_up_bigscrapy_project',
-        command='echo "hello world" > summary.txt'
+    tear_down_bigscrapy_project_task = SSHOperator(
+        ssh_conn_id='ssh_big_airflow',
+        task_id='tear_down_bigscrapy_project',
+        command="""
+        docker rm -f bigscrapy_projects_airflow
+        """
     )
+
+    set_up_bigscrapy_project_task >> tear_down_bigscrapy_project_task
